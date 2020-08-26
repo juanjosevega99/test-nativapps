76c4627c0ab1f85b45f581e1c5d023a20c26cf99
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-26 14:17:41 -0500

    [FIX] get hello worl

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from "@nestjs/config";
+import { StudentsModule } from './students/students.module';
+
+import { CoursesController } from './courses/courses.controller';
+import { CoursesService } from './courses/courses.service';
+import { CoursesModule } from './courses/courses.module';
+
 @Module({
-  imports: [],
+  imports: [
+    // ConfigModule.forRoot(),
+    StudentsModule,
+    CoursesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
