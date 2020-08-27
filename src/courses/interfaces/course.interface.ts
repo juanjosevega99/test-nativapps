1ed9ec84105176c47d5cac20d2d3d88f4e0c1f1f
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-27 10:20:40 -0500

    [FIX] name interface

--- src/courses/interfaces/course.interface.ts
+++ src/courses/interfaces/course.interface.ts
@@ -1,6 +1,6 @@
 import { Document } from 'mongoose';
 
-export interface Product extends Document {
+export interface Course extends Document {
   name: string;
   schedule: string;
   dateStart: Date;
