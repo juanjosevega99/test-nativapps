daea8c4332ffeca92f1d2e48aed4d51a4caf556a
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-27 10:39:43 -0500

    [FIX] prettier

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CourseSchema } from './schemas/course.schema';
+import { CoursesController } from './courses.controller';
+import { CoursesService } from './courses.service';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Course', schema: CourseSchema }]),
+  ],
+  controllers: [CoursesController],
+  providers: [CoursesService],
+})
 export class CoursesModule {}
