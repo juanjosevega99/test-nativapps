7f48c26d9c8cb7de69d600d8ef1b5d4c33d391d5
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-27 07:52:50 -0500

    [FEATURE] controller student

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus, Get, Res, Post, Body } from '@nestjs/common';
+
+import { CreateStudentDTO } from './dto/student.dto'
+import { StudentsService } from './students.service'
 
 @Controller('students')
-export class StudentsController {}
+export class StudentsController {
+
+  constructor(private studentsService: StudentsService) {}
+
+  @Get('/')
+  async getProducts(@Res() res) {
+    const products = await this.studentsService.getStudents()
+    return res.status(HttpStatus.OK).json(products)
+  }
+
+  @Post('/create')
+  async createStudent(@Res() res, @Body() createStudentDTO: CreateStudentDTO) {
+    const student = await this.studentsService.createStudent(createStudentDTO)
+    return res.status(HttpStatus.CREATED).json({
+      message: 'Student Successfully Created',
+      student
+    })
+  }
+}
