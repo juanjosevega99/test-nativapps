eee58bd7900cd0be57b03767100418eb9d39ded9
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-27 11:54:08 -0500

    [FEATURE] assign student to course module

--- src/students/students.module.ts
+++ src/students/students.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { StudentsController } from './students.controller';
 import { StudentsService } from './students.service';
 import { StudentSchema } from './schemas/student.schema';
+import { CourseSchema } from 'src/courses/schemas/course.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Student', schema: StudentSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Student', schema: StudentSchema },
+      { name: 'Course', schema: CourseSchema },
+    ]),
   ],
   controllers: [StudentsController],
   providers: [StudentsService],
