57f7ec39bc6ea41c46ed93b12b79d3128022d0d7
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-27 07:52:35 -0500

    [FEATURE] create students

--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+
+import { Student } from './interface/student.interface';
+import { CreateStudentDTO } from './dto/student.dto';
 
 @Injectable()
-export class StudentsService {}
+export class StudentsService {
+
+  constructor(@InjectModel('Student') private studentModel: Model<Student>) {}
+
+  async getStudents(): Promise<Student[]> {
+    const students = await this.studentModel.find()
+    return students
+  }
+
+  async createStudent(createStudentDTO: CreateStudentDTO): Promise<Student> {
+    const createdStudent = new this.studentModel(createStudentDTO)
+    return await createdStudent.save()
+  }
+}
