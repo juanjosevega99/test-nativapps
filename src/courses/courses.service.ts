7fb711ba8ad66d08131add761fdab9e62335ca42
Juna Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-27 23:22:47 -0500

    [FEATURE] method top 3

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -5,6 +5,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Course } from './interfaces/course.interface';
 import { CreateCourseDTO } from './dto/course.dto';
 
+import * as moment from 'moment';
+
 @Injectable()
 export class CoursesService {
   constructor(@InjectModel('Course') private courseModel: Model<Course>) {}
@@ -12,6 +14,24 @@ export class CoursesService {
   async getCourses(): Promise<Course[]> {
     const courses = await this.courseModel.find();
     return courses;
+  }
+  
+  async top() {
+    const sixMonthsAgo = moment().subtract(6, 'months').toISOString()
+    const courses = await this.courseModel.find({ dateEnd: { $gte: new Date(sixMonthsAgo) } });
+
+    let topCourses = []
+    courses.forEach((item) => {
+      if (item.students >= 0) {
+        topCourses.push(item)
+        topCourses.sort(function(a, b) {
+          return a - b
+        })
+      }
+    })
+    topCourses = topCourses.filter((students, idx) => idx < 3)
+
+    return topCourses
   }
 
   async createCourse(createCourseDTO: CreateCourseDTO): Promise<Course> {
